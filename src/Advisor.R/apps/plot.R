f8ad76ec2d399c97aaf6a061d3c515f10b4a6621
krdr <kdrelczuk@hotmail.com>
2014-05-26 22:09:01 +0200

    plot output completed

--- src/Advisor.R/apps/plot.R
+++ src/Advisor.R/apps/plot.R
@@ -1,7 +1,12 @@
 app <- function(env)
 {
+  req <- Rook::Request$new(env)
+  res <- Rook::Response$new()
+  
   data = rnorm(n=1000,mean=0,sd=10)
   png(file = "/var/www/html/plot.png", bg = "transparent")
   hist(data,col='red')
   dev.off()
+  res.write(paste('Plot computed on ',Sys.time()))
+  res$finish()
 }
