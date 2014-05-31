db1bae78c9a992517433f570df35ddce764051d9
krdr <kdrelczuk@hotmail.com>
2014-05-31 22:28:23 +0200

    R error handling

--- src/Advisor.R/apps/plot.R
+++ src/Advisor.R/apps/plot.R
@@ -5,18 +5,31 @@ app <- function(env)
   req <- Rook::Request$new(env)
   res <- Rook::Response$new()
   
-  query = Utils$parse_query(req$query_string())
-  symbol = query$id
-  x = as.numeric(query$x)
-  y = as.numeric(query$y)
-  pstart = as.Date(query$pstart)
-  pstop = as.Date(query$pstop)
-  data = GetAdvisorData(symbol,pstart,pstop)
-  
-  png(file='test.png',width=x,height=y)
-  chartSeries(data, TA = 'addVo();addBBands();addCCI();addSMI()')
-  dev.off()
-  
-  res$write(Sys.time()-t1)
-  res$finish()
+  result = tryCatch(
+    {
+      query = Utils$parse_query(req$query_string())
+      symbol = query$id
+      x = as.numeric(query$x)
+      y = as.numeric(query$y)
+      pstart = as.Date(query$pstart)
+      pstop = as.Date(query$pstop)
+      data = GetAdvisorData(symbol,pstart,pstop)
+      
+      png(file='test.png',width=x,height=y)
+      chartSeries(data, TA = 'addVo();addBBands();addCCI();addSMI()')
+      dev.off()
+    }
+    , warning = function(war)
+    {
+      logwarn('plot application',war)
+    }
+    , error = function(err)
+    {
+      logerror('plot application',err)  
+    }
+    , finally = 
+    {
+      res$write(Sys.time()-t1)
+      res$finish()
+    })
 }
