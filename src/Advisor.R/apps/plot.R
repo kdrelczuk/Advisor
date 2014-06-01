b7c07a1dfd26b78bc7f82b58063d335323017013
krdr <kdrelczuk@hotmail.com>
2014-06-01 18:14:11 +0200

    r plot cache

--- src/Advisor.R/apps/plot.R
+++ src/Advisor.R/apps/plot.R
@@ -1,14 +1,19 @@
 app <- function(env)
 {
+  # initialize veriable
   t1 = Sys.time()
+  cached = T
   reqId = trunc(runif(1, 1000000, 9999999))
-  loginfo(paste("request for plot made - id:",reqId))
+  
+  #start processing request
+  loginfo(paste("* request for plot made (id:",reqId,")",sep=""))
   req <- Rook::Request$new(env)
   res <- Rook::Response$new()
   
   result = tryCatch(
     {
       query = Utils$parse_query(req$query_string())
+      symbolName = query$name
       symbol = query$id
       x = as.numeric(query$x)
       y = as.numeric(query$y)
@@ -16,21 +21,39 @@ app <- function(env)
       pstop = as.Date(query$pstop)
       data = GetAdvisorData(symbol,pstart,pstop)
       
-      png(file='/var/www/html/test.png',width=x,height=y)
-      chartSeries(data, TA = 'addVo();addBBands();addCCI();addSMI()',theme='white')
-      dev.off()
+      catName = '/var/www/html'
+      fileName = paste('plot_id',symbol
+                       ,'_name',symbolName
+                       ,'_x',as.character(x)
+                       ,'_y',as.character(y)
+                       ,'_pstart',as.character(pstart)
+                       ,'_pstop',as.character(pstop)
+                       ,'.png',sep='')
+      fullPlotPath = paste(catName,fileName,sep='/')
+      if (!file.exists(fullPlotPath))
+      {
+        cached = F
+        png(file='fullPlotPath',width=x,height=y)
+        chartSeries(data, TA = 'addVo();addBBands();addMACD()',theme='white',name=symbolName)
+        dev.off()
+      }
     }
     , warning = function(war)
     {
-      logwarn(paste('plot application',war))
+      logwarn(paste(reqId,as.character(war)))
       res$write(war)
     }
     , error = function(err)
     {
-      logerror(paste('plot application',err))
+      logerror(paste(reqId,as.character(err)))
       res$write(err)
     })
-  
-  res$write(paste("request with id:",reqId,"ended in ",as.character(Sys.time()-t1)))
+  logMessage = paste("request for plot ended in with id:",reqId,"ended in ",as.character(Sys.time()-t1)) 
+  if (cached)
+  {
+    paste(logMessage,'(from cache)')
+  }
+  loginfo(logMessage)
+  res$write()
   res$finish()
 }
