ab32f78d49b943822fa27fde2069ca0b5341f1be
krdr <kdrelczuk@hotmail.com>
2014-07-01 23:39:14 +0200

    aws init

---
+++ deploy/AWSInit/GetAddresses.ps1
@@ -0,0 +1,10 @@
+﻿Set-AWSCredentials -AccessKey AKIAICHBP6K4MHHIKWZQ -SecretKey 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 -StoreAs AdvisorAWS
+Set-AWSCredentials -StoredCredentials AdvisorAWS
+
+
+$runningInstances = (Get-EC2Instance -Region us-west-2).Instances | ?{ $_.State.Name -eq 'running' }  
+$runningInstances | %{
+   $_.PublicIpAddress | Out-File "C:\rserversIps.txt" -Append
+   $_.PublicDnsName | Out-File "C:\rserversDns.txt" -Append
+}
+# Get-Command -Module AWSPowerShell | Select-String region
