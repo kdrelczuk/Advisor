c8fd20b5cba070c02a10ac6f827e5e8de26d4a11
krdr <kdrelczuk@hotmail.com>
2014-07-01 23:51:26 +0200

    path 2

--- deploy/AWSInit/GetAddresses.ps1
+++ deploy/AWSInit/GetAddresses.ps1
@@ -1,12 +1,12 @@
 ﻿Set-AWSCredentials -AccessKey AKIAICHBP6K4MHHIKWZQ -SecretKey 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 -StoreAs AdvisorAWS
 Set-AWSCredentials -StoredCredentials AdvisorAWS
 
-$path = ".\artefacts"
+$path = ".\artefacts\rservers"
 New-Item -ItemType directory -Path $path
 
 $runningInstances = (Get-EC2Instance -Region us-west-2).Instances | ?{ $_.State.Name -eq 'running' }  
 $runningInstances | %{
-   $_.PublicIpAddress | Out-File ($path + "\rserversIps.txt") -Append
-   $_.PublicDnsName | Out-File ($path + "\rserversDns.txt") -Append
+   $_.PublicIpAddress | Out-File ($path + "\ips.txt") -Append
+   $_.PublicDnsName | Out-File ($path + "\dns.txt") -Append
 }
 # Get-Command -Module AWSPowerShell | Select-String region
