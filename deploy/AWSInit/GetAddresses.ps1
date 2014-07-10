65b58696684d21cdead664922b958d4bf1c3745c
krdr <kdrelczuk@hotmail.com>
2014-07-10 21:44:55 +0200

    powershell update

--- deploy/AWSInit/GetAddresses.ps1
+++ deploy/AWSInit/GetAddresses.ps1
@@ -4,7 +4,7 @@ Set-AWSCredentials -StoredCredentials AdvisorAWS
 $path = ".\artefacts\rservers"
 New-Item -ItemType directory -Path $path
 
-$runningInstances = (Get-EC2Instance -Region us-west-2).Instances | ?{ $_.State.Name -eq 'running' }  
+$runningInstances = (Get-EC2Instance -Region us-west-2).Instances | ?{ ($_.State.Name -eq 'running') -and ($_.KeyName -eq 'rserver01') }  
 $runningInstances | %{
    $_.PublicIpAddress | Out-File ($path + "\ips.txt") -Append
    $_.PublicDnsName | Out-File ($path + "\dns.txt") -Append
