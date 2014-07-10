05c9e96f91dc8d378d13f6c862c110a51baf7adf
krdr <kdrelczuk@hotmail.com>
2014-07-10 23:59:53 +0200

    CreateSFPDScript.ps1

--- deploy/Advisor.R/CreateSFPDScript.ps1
+++ deploy/Advisor.R/CreateSFPDScript.ps1
@@ -4,15 +4,15 @@ $0 = Test-Path $path
 if (!$0) { New-Item -ItemType directory -Path $path }
 else { Remove-Item ($path + '\*') }
 
-'option batch abort' | Out-File ($path + "\spdf.txt") -Append
-'option confirm off' | Out-File ($path + "\spdf.txt") -Append
+'option batch abort' | Out-File ($path + "\sfpd.txt") -Append
+'option confirm off' | Out-File ($path + "\sfpd.txt") -Append
 
 $a = Get-Content 'rservers\dns.txt'
 
 $a | 
 %{ 
-    ('open scp://ubuntu@' + $_ + ' -privatekey=keys/rserver01.ppk') | Out-File ($path + "\spdf.txt") -Append
-    'put src\ /Advisor.R/' | Out-File ($path + "\spdf.txt") -Append
+    ('open scp://ubuntu@' + $_ + ' -privatekey=keys/rserver01.ppk') | Out-File ($path + "\sfpd.txt") -Append
+    'put src\ /Advisor.R/' | Out-File ($path + "\sfpd.txt") -Append
 }
 
-'exit' | Out-File ($path + "\spdf.txt") -Append
+'exit' | Out-File ($path + "\sfpd.txt") -Append
